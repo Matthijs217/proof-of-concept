5d377a107af41f5c2ec1d4aa956ef28af0b35ed4
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-06-11 13:20:10 +0200

    Navigatie PE begin #7

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
 const API_PEOPLE_ENDPOINT = "/api/people";
+
+document.querySelector('.menu-button').addEventListener('click', function(ev) {
+  document.querySelector('.footer-nav').classList.add('target')
+  document.querySelector('.footer-nav-container').classList.add('show')
+  document.querySelector('.menu-close').classList.add('show')
+  document.querySelector('.footer-nav').focus();
+  ev.preventDefault();
+})
+
+document.querySelector('.menu-close').addEventListener('click', function(ev) {
+  document.querySelector('.footer-nav').classList.remove('target')
+  document.querySelector('.footer-nav-container').classList.remove('show')
+  document.querySelector('.menu-close').classList.remove('show')
+  document.body.focus();
+  ev.preventDefault();
+})
