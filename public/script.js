4b48b919058532f0e0f8122e16df569d7d973d2c
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-06-13 11:12:17 +0200

    feature:responsive-header-nav #7
    Navigatie is mooi responsive nu. Je hebt op mobile gewoon je hamburger menu en vanaf desktop zie je gewoon de hele navigatie in de header.

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
 const API_PEOPLE_ENDPOINT = "/api/people";
 
 document.querySelector('.menu-button').addEventListener('click', function(ev) {
-  document.querySelector('.footer-nav').classList.add('target')
-  document.querySelector('.footer-nav-container').classList.add('show')
+  document.querySelector('.header-nav').classList.add('target')
+  document.querySelector('.header-nav-container').classList.add('show')
   document.querySelector('.menu-close').classList.add('show')
-  document.querySelector('.footer-nav').focus();
+  document.querySelector('.header-nav').focus();
   ev.preventDefault();
 })
 
 document.querySelector('.menu-close').addEventListener('click', function(ev) {
-  document.querySelector('.footer-nav').classList.remove('target')
-  document.querySelector('.footer-nav-container').classList.remove('show')
+  document.querySelector('.header-nav').classList.remove('target')
+  document.querySelector('.header-nav-container').classList.remove('show')
   document.querySelector('.menu-close').classList.remove('show')
   document.body.focus();
   ev.preventDefault();
